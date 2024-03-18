be1b1a1349de6c91d1e078c9c26a2fff29ebd951
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-03-18 16:56:00 +0000

    Move files into subdir

--- cmd/calculator/operations.go
+++
@@ -1,9 +0,0 @@
-package calculator
-
-func add(num1, num2 int) int {
-	return num1 + num2
-}
-
-func subtract(num1, num2 int) int {
-	return num1 - num2
-}
--- go.mod
+++
@@ -1,5 +0,0 @@
-module go-cli-app
-
-go 1.22.1
-
-require golang.org/x/text v0.14.0
