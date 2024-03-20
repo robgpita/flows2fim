10318ad24b64c2ce0ee4f71dae2d20f6e6643871
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-03-20 15:07:40 +0000

    Update cmd references

--- flows2fim/main.go
+++ flows2fim/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"flows2fim/cmd/calculator"
-	"flows2fim/cmd/hello"
+	"flows2fim/cmd/controls"
+	"flows2fim/cmd/fim"
 	"flows2fim/internal/config"
 )
 
@@ -14,15 +14,15 @@ func main() {
 	config.LoadConfig()
 
 	if len(os.Args) < 2 {
-		fmt.Println("Please provide a subcommand: hello or calculator")
+		fmt.Println("Please provide a subcommand: controls | fim")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
-	case "hello":
-		hello.Run(os.Args[2])
-	case "calculator":
-		calculator.Run(os.Args[2:])
+	case "controls":
+		controls.Run(os.Args[2:])
+	case "fim":
+		fim.Run(os.Args[2:])
 	default:
 		fmt.Println("Unknown subcommand")
 		os.Exit(1)
