8a6db80c6a5f46e2e019b1a506c3dbc3a010120e
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-04-02 16:29:41 +0000

    Add mac build script

---
+++ scripts/build-mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The script must be executed from the root of the repository
+set -eo pipefail
+
+echo "Building for Mac..."
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -buildvcs=false -o builds/darwin-arm64/flows2fim main.go
+echo "Mac build completed."
