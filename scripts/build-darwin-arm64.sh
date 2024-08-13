641de63a0941edad0a28d5a7b8e84a28d86fc6bf
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-08-13 16:34:04 +0000

    Make build scripts names specific

--- scripts/build-darwin-arm64.sh
+++ scripts/build-darwin-arm64.sh
@@ -3,6 +3,6 @@
 # The script must be executed from the root of the repository
 set -eo pipefail
 
-echo "Building for Mac..."
+echo "Building for Darwin (MacOS) ARM64..."
 CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -buildvcs=false -o builds/darwin-arm64/flows2fim main.go
 echo "Mac build completed."
