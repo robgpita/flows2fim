87b75e7297b9d7af1edcdc130c1ff4f0f55d5a84
rob.pita <rob.gonzalez-pita@ertcorp.com>
2025-01-24 11:35:17 -0700

    update workflow

--- scripts/test-matrix-linux-amd64.sh
+++ scripts/test-matrix-linux-amd64.sh
@@ -1,19 +1,18 @@
-#! /bin/bash
+#!/bin/bash
 
 # Implement command line arg that can take controls, fim, validate or nothing (if nothing run all)
 
-flows_file=[ a.csv, b.txt , c]
+# 
 
-echo "Testing flows2fim controls"
+echo ">>>> Testing flows2fim controls"
+flows2fim controls 
+echo "<<<< flows2fim controls tests pass"
 
-for file in ${@flows_file}
-    flows2fim controls -f ${i}
-
-echo "flows2fim controls test pass"
-
-
+echo ">>>> Testing flows2fim fim"
 flows2fim fim 
+echo "<<<< flows2fim controls tests pass"
 
+echo ">>>> Testing flows2fim validate"
 flows2fim validate
-
+echo "<<<< flows2fim validate tests pass"
 
